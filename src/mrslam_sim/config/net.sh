0ab60d6ef68207f75f9cd4be54c7b541a5ea6333
Maxim Kuzmin <max_kuzmin@outlook.com>
2017-11-17 00:20:27 +0300

    Added ad-hoc network script

---
+++ src/mrslam_sim/config/net.sh
@@ -0,0 +1,11 @@
+export ROS_IP=169.254.34.2
+export ROS_MASTER_URI=http://169.254.34.2:11112
+
+sudo ip link set wlan0 down 
+sudo iwconfig wlan0 mode ad-hoc 
+sudo iwconfig wlan0 channel 4 
+sudo iwconfig wlan0 essid 'ROS1' 
+sudo iwconfig wlan0 key 1234267895
+sudo ip link set wlan0 up 
+sudo ip addr add 169.254.34.2/16 dev wlan0
+sudo route add -net 224.0.0.0 netmask 224.0.0.0 wlan0
