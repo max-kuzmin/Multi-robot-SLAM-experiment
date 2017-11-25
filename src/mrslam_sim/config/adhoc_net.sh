20f3bcbb2673a06d5f3af570a6eaa7a596ee33a5
Maxim Kuzmin <max_kuzmin@outlook.com>
2017-11-25 12:02:29 +0300

    Unified config names

--- src/mrslam_sim/config/adhoc_net.sh
+++ src/mrslam_sim/config/adhoc_net.sh
@@ -1,6 +1,3 @@
-export ROS_IP=169.254.34.2
-export ROS_MASTER_URI=http://169.254.34.2:11112
-
 sudo ip link set wlan0 down 
 sudo iwconfig wlan0 mode ad-hoc 
 sudo iwconfig wlan0 channel 4 
@@ -9,3 +6,6 @@ sudo iwconfig wlan0 key 1234267895
 sudo ip link set wlan0 up 
 sudo ip addr add 169.254.34.2/16 dev wlan0
 sudo route add -net 224.0.0.0 netmask 224.0.0.0 wlan0
+
+export ROS_IP=169.254.34.2
+export ROS_MASTER_URI=http://169.254.34.2:11112
