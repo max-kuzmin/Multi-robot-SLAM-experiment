c2d24616784b70cfc6c745dabdca97831ca9e0c3
Max Kuzmin <max_kuzmin@outlook.com>
2018-03-29 11:56:46 -0700

    multirobot_map_merge works with multimaster

--- src/mrslam_sim/scripts/install_ssh_keys.sh
+++ src/mrslam_sim/scripts/install_ssh_keys.sh
@@ -1,3 +1,3 @@
 ssh-keygen -t rsa
-ssh-copy-id max@192.168.217.129
-ssh-copy-id max@192.168.217.130
+ssh-copy-id max@192.168.217.131
+ssh-copy-id max@192.168.217.132
--- src/mrslam_sim/scripts/mm_gmapping_mapmerging_step3.sh
+++ src/mrslam_sim/scripts/mm_gmapping_mapmerging_step3.sh
@@ -3,16 +3,16 @@ source /opt/ros/kinetic/setup.bash;
 source /home/max/Desktop/ROS-SLAM-playground/devel/setup.bash;
 gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_mapmerging_step3.launch robot_name:=robot1'
 
-ssh max@192.168.217.129 "
+ssh max@192.168.217.131 "
 export DISPLAY=:0;
-export ROS_IP=192.168.217.129;
+export ROS_IP=192.168.217.131;
 source /opt/ros/kinetic/setup.bash;
 source /home/max/Desktop/ROS-SLAM-playground/devel/setup.bash;
 gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_mapmerging_step3.launch robot_name:=robot2'"
 
-ssh max@192.168.217.130 "
+ssh max@192.168.217.132 "
 export DISPLAY=:0;
-export ROS_IP=192.168.217.130;
+export ROS_IP=192.168.217.132;
 source /opt/ros/kinetic/setup.bash;
 source /home/max/Desktop/ROS-SLAM-playground/devel/setup.bash;
 gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_mapmerging_step3.launch robot_name:=robot3'"
