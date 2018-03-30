03504ee24272cd42f4d51e668ef3a2f698b8aa23
Max Kuzmin <max_kuzmin@outlook.com>
2018-03-30 00:37:08 -0700

    Fixed some multimaster scripts

--- src/mrslam_sim/scripts/mm_gmapping_multirobotmapmerge_step1.sh
+++ src/mrslam_sim/scripts/mm_gmapping_multirobotmapmerge_step1.sh
@@ -1,18 +1,18 @@
 export ROS_IP=192.168.217.128;
 source /opt/ros/kinetic/setup.bash;
 source /home/max/Desktop/ROS-SLAM-playground/devel/setup.bash;
-gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_multirobotmapmerge_step1.launch x:=0 y:=0 robot_name:=robot1'
+gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_multirobotmapmerge_step1.launch x:=6 y:=-6 robot_name:=robot1'
 
 ssh max@192.168.217.131 "
 export DISPLAY=:0;
 export ROS_IP=192.168.217.131;
 source /opt/ros/kinetic/setup.bash;
 source /home/max/Desktop/ROS-SLAM-playground/devel/setup.bash;
-gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_multirobotmapmerge_step1.launch x:=1 y:=1 robot_name:=robot2'"
+gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_multirobotmapmerge_step1.launch x:=5 y:=-5 robot_name:=robot2'"
 
 ssh max@192.168.217.132 "
 export DISPLAY=:0;
 export ROS_IP=192.168.217.132;
 source /opt/ros/kinetic/setup.bash;
 source /home/max/Desktop/ROS-SLAM-playground/devel/setup.bash;
-gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_multirobotmapmerge_step1.launch x:=-1 y:=-1 robot_name:=robot3'"
+gnome-terminal -e 'roslaunch mrslam_sim mm_gmapping_multirobotmapmerge_step1.launch x:=5 y:=-6 robot_name:=robot3'"
