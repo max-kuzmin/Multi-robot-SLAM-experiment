0ce39782559524f8629d1013c26871754b95bd7a
Max Kuzmin <max_kuzmin@outlook.com>
2018-04-23 15:45:08 -0700

    graphslam2d partially works

--- src/mrslam_sim/scripts/ms_mrptgraphslam2d_robot0.sh
+++ src/mrslam_sim/scripts/ms_mrptgraphslam2d_robot0.sh
@@ -1,6 +1,8 @@
 source /opt/ros/kinetic/setup.bash;
 source devel/setup.bash;
-export ROS_MASTER_URI=http://localhost:11110
+export ROS_IP=192.168.218.1
+export ROS_MASTER_URI=http://192.168.218.1:11110
+export GAZEBO_MASTER_URI=http://192.168.218.1:11100
 #gnome-terminal -e 'roscore -p 11110'
 #sleep 3
 gnome-terminal -e 'roslaunch -p 11110 mrslam_sim ms_mrptgraphslam2d_step1.launch x:=4 y:=-5 r_id:=0 r_count:=2'
--- src/mrslam_sim/scripts/ms_mrptgraphslam2d_robot1.sh
+++ src/mrslam_sim/scripts/ms_mrptgraphslam2d_robot1.sh
@@ -1,6 +1,8 @@
 source /opt/ros/kinetic/setup.bash;
 source devel/setup.bash;
-export ROS_MASTER_URI=http://localhost:11111
+export ROS_IP=192.168.219.1
+export ROS_MASTER_URI=http://192.168.219.1:11111
+export GAZEBO_MASTER_URI=http://192.168.219.1:11101
 #gnome-terminal -e 'roscore -p 11111'
 #sleep 3
 gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrptgraphslam2d_step1.launch x:=4 y:=2 r_id:=1 r_count:=2'
