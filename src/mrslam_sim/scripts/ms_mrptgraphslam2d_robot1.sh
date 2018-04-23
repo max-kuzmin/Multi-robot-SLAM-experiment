abece1e3ed5b666a3c9c4362a2d2c4cb764dba53
Max Kuzmin <max_kuzmin@outlook.com>
2018-04-23 04:22:59 -0700

    Fixes

--- src/mrslam_sim/scripts/ms_mrptgraphslam2d_robot1.sh
+++ src/mrslam_sim/scripts/ms_mrptgraphslam2d_robot1.sh
@@ -1,8 +1,8 @@
 source /opt/ros/kinetic/setup.bash;
 source devel/setup.bash;
 export ROS_MASTER_URI=http://localhost:11111
-gnome-terminal -e 'roscore -p 11111'
-sleep 3
+#gnome-terminal -e 'roscore -p 11111'
+#sleep 3
 gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrptgraphslam2d_step1.launch x:=4 y:=2 r_id:=1 r_count:=2'
 sleep 7
-gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrptgraphslam2d_step2.launch'
+gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrptgraphslam2d_step2.launch r_id:=1'
