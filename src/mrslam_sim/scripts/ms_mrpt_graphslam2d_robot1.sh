93e324ad4f74588a5fb72e648192c81b8217a503
Max Kuzmin <max_kuzmin@outlook.com>
2018-04-26 04:37:08 -0700

    Initially added mrpt_rbpf

--- src/mrslam_sim/scripts/ms_mrpt_graphslam2d_robot1.sh
+++ src/mrslam_sim/scripts/ms_mrpt_graphslam2d_robot1.sh
@@ -5,6 +5,6 @@ export ROS_MASTER_URI=http://192.168.219.1:11111
 export GAZEBO_MASTER_URI=http://192.168.219.1:11101
 #gnome-terminal -e 'roscore -p 11111'
 #sleep 3
-gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrptgraphslam2d_step1.launch x:=4 y:=-5 r_id:=1 r_count:=2'
+gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrpt_graphslam2d_step1.launch x:=4 y:=-5 r_id:=1 r_count:=2'
 sleep 7
-gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrptgraphslam2d_step2.launch r_id:=1'
+gnome-terminal -e 'roslaunch -p 11111 mrslam_sim ms_mrpt_graphslam2d_step2.launch r_id:=1'
